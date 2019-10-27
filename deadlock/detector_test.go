754e5e69ec71a7cec4355ef76052005e459b7515
TAKAHASHI Yuto <ytaka23dev@gmail.com>
2019-10-28 00:09:35 +0900

    Add tests

---
+++ deadlock/detector_test.go
@@ -0,0 +1,68 @@
+package deadlock_test
+
+import (
+	"testing"
+
+	"github.com/y-taka-23/ddsv-go/deadlock"
+	"github.com/y-taka-23/ddsv-go/deadlock/rule"
+)
+
+func TestDetect(t *testing.T) {
+
+	tests := []struct {
+		name string
+		in   deadlock.System
+		want summary
+	}{
+		{
+			"1-step",
+			deadlock.NewSystem().
+				Register("P", deadlock.NewProcess().
+					EnterAt("0").
+					Define(rule.At("0").MoveTo("1"))),
+			summary{state: 2, trans: 1},
+		},
+		{
+			"2-step",
+			deadlock.NewSystem().
+				Register("P", deadlock.NewProcess().
+					EnterAt("0").
+					Define(rule.At("0").MoveTo("1")).
+					Define(rule.At("1").MoveTo("2"))),
+			summary{state: 3, trans: 2},
+		},
+		{
+			"1-step 1-step",
+			deadlock.NewSystem().
+				Register("P", deadlock.NewProcess().
+					EnterAt("0").
+					Define(rule.At("0").MoveTo("1"))).
+				Register("Q", deadlock.NewProcess().
+					EnterAt("0").
+					Define(rule.At("0").MoveTo("1"))),
+			summary{state: 4, trans: 4},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := summarize(deadlock.NewDetector().Detect(tt.in))
+			if got != tt.want {
+				t.Fatalf("want %+v, but %+v", tt.want, got)
+			}
+		})
+	}
+
+}
+
+type summary struct {
+	state int
+	trans int
+}
+
+func summarize(rp deadlock.Report) summary {
+	return summary{
+		state: len(rp.Visited()),
+		trans: len(rp.Transited()),
+	}
+}
